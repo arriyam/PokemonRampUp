7b6e8865254197d6a27081a071fa2e668d27ddcf
Thushen Mohanarajah <67329242+arriyam@users.noreply.github.com>
2023-02-14 15:00:14 -0500

    Update Network.swift file with feedback
    Co-authored-by: Judar Lima <judar.rslima@gmail.com>

--- PokemonRampUp/Model/Network.swift
+++ PokemonRampUp/Model/Network.swift
@@ -8,22 +8,31 @@
 import Foundation
 
 // TO-DO [POKEMON-0012] Create unit test for Network model
-class Network{
-    
-    static func loadJSONObject<T: Decodable>(stringURL: String, type: T.Type) async throws -> T{
-        guard let url = URL(string: stringURL) else {
-            throw NetworkError.invalidURL
-        }
-        
-        guard let (data, _) = try? await URLSession.shared.data(from: url) else{
-            throw NetworkError.requestError
-        }
-        
-        let decoder = JSONDecoder()
-        guard let objectT = try? decoder.decode(T.self, from: data) else {
-            throw NetworkError.dateParseError
-        }
-        
-        return objectT
-    }
+protocol Network {
+    func loadJSONObject<T: Decodable>(stringURL: String, type: T.Type) async throws -> T
 }
+
+struct DefaultNetwork: Network {
+    private let urlSession: URLSession
+    
+    init(urlSession: URLSession = .shared) {
+        self.urlSession = urlSession
+    }
+
+     func loadJSONObject<T: Decodable>(stringURL: String, type: T.Type) async throws -> T {
+         guard let url = URL(string: stringURL) else {
+             throw NetworkError.invalidURL
+         }
+
+         guard let (data, _) = try? await urlSession.data(from: url) else{
+             throw NetworkError.requestError
+         }
+
+         let decoder = JSONDecoder()
+         guard let objectT = try? decoder.decode(T.self, from: data) else {
+             throw NetworkError.dateParseError
+         }
+
+         return objectT
+     }
+ }
