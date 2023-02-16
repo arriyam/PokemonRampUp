e97378034731f7f3169e8366d47ceb6909b6cd54
Thushen <thushenarriyam@gmail.com>
2023-02-16 16:57:34 -0500

    Added dreamworld url to Sprites

--- PokemonRampUp/Model/Sprites.swift
+++ PokemonRampUp/Model/Sprites.swift
@@ -10,24 +10,32 @@ import Foundation
 struct Sprites {
     let frontDefault: String?
     let frontShiny: String?
+    let frontDreamWorld: String?
 
     init(frontDefault: String,
-         frontShiny: String
+         frontShiny: String,
+         frontDreamWorld: String
     ) {
         self.frontDefault = frontDefault
         self.frontShiny = frontShiny
+        self.frontDreamWorld = frontDreamWorld
     }
 }
 
 extension Sprites: Decodable {
     enum CodingKeys: String, CodingKey {
-        case frontDefault = "front_default"
+        case frontDefault, frontDreamWorld = "front_default"
         case frontShiny = "front_shiny"
+        case other
+        case dreamWorld = "dream_world"
     }
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.frontDefault = try container.decodeIfPresent(String.self, forKey: .frontDefault)
         self.frontShiny = try container.decodeIfPresent(String.self, forKey: .frontShiny)
+        let otherContainer = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .other)
+        let dreamWorldContainer = try otherContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .dreamWorld)
+        self.frontDreamWorld = try dreamWorldContainer.decodeIfPresent(String.self, forKey: .frontDreamWorld)
     }
 }
