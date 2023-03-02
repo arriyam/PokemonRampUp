ee99473853f03e608e58c3b2c2e89cef2be46e57
Thushen <thushenarriyam@gmail.com>
2023-03-02 16:18:32 -0500

    Created Details page that contains a UIImage and two UILabels

---
+++ PokemonRampUp/ViewController/DetailsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailsViewController.swift
+//  PokemonRampUp
+//
+//  Created by Thushen Mohanarajah on 2023-03-02.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
