a7ba1907eaab586c5c8a9e36a08e4062e256bf6f
Thushen <thushenarriyam@gmail.com>
2023-03-16 15:16:57 -0400

    Almost completed MockNetwork class

---
+++ PokemonRampUpTests/Mocks/NetworkMock.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkMock.swift
+//  PokemonRampUpTests
+//
+//  Created by Thushen Mohanarajah on 2023-03-16.
+//
+
+import Foundation
+import UIKit
+@testable import PokemonRampUp
+
+enum GenericError: Error {
+    case genericError
+}
+
+class NetworkMock: Network {
+    private var loadJSONObjectSuccess = true
+    private var loadUIImagetSuccess = true
+    
+    
+    func loadJSONObject<T: Decodable>(stringURL: String, type: T.Type) async throws -> T {
+        if (loadJSONObjectSuccess){
+            
+        }
+        throw GenericError.genericError
+    }
+    
+    func loadUIImage(urlString: String?) async throws -> UIImage? {
+        if (loadUIImagetSuccess){
+            return UIImage(named: "pichu_image")
+        }
+        throw GenericError.genericError
+
+    }
+    
+    func setloadJSONObjectSuccess(success: Bool) {
+        self.loadJSONObjectSuccess = success
+    }
+    
+    func setloadUIImagetSuccess(success: Bool) {
+        self.loadUIImagetSuccess = success
+    }
+    
+    
+}
